e5aa3277e0a751f096d1b4a688b12a184c3f085e
shua415 <siyinghuangnz@gmail.com>
2023-11-21 11:17:23 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 # multi-select widget that allows users to pick fruits (widgets are index by default)
 my_fruit_list = my_fruit_list.set_index('Fruit')    # set index to the column values of Fruit 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
 # Display the table on the page.
