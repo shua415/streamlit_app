3a0c65c979c7dce560029ffc5c5a175ef613e70f
shua415 <siyinghuangnz@gmail.com>
2023-11-21 11:03:50 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit 
 
-streamlit.title('My Parents New Healthy Diner!!!') 
+streamlit.title('My Parents New Healthy Diner') 
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
