dbda5e4393478015d0a16c3060a8d5407e431fd9
shua415 <siyinghuangnz@gmail.com>
2023-11-21 11:11:22 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,11 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
+# reading csv text file 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+# multi-select widget that allows users to pick fruits 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+my_fruit_list = my_fruit_list.set_index('Fruit')    # set index to the column values of Fruit 
+# Display the table on the page.
