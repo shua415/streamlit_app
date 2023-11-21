c624131b95500e47df5e9354b25633cdb078a971
shua415 <siyinghuangnz@gmail.com>
2023-11-22 12:26:04 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,10 @@ streamlit.dataframe(my_fruit_list)
 
 # displya fruityvice api response 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")  # gets data from the website 
 
 # normalises the data by applying a standard format to the table 
