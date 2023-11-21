0d11f01ce40af440c4a396a104aca1533aeba515
shua415 <siyinghuangnz@gmail.com>
2023-11-22 12:18:50 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,12 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
-# displya fruityvice api response 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
-
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
+# displya fruityvice api response 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
 
