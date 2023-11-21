169f30ba143109350a623eaff63952fb28ff5eb1
Siying Huang <103796384+shua415@users.noreply.github.com>
2023-11-22 12:15:02 +1300

    fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,9 @@ streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show) 
 
 
+# displya fruityvice api response 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
