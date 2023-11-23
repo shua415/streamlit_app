cc8768b880bbfa632b80a980da7fefd2e03cdc86
shua415 <siyinghuangnz@gmail.com>
2023-11-24 09:44:41 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
+# FUNCTION that returns the info of the requested fruit choice 
+def get_fruityvice_data(this_fruit_choice): 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # normalises the data by applying a standard format to the table 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized 
+
 # displya fruityvice api response 
 streamlit.header("Fruityvice Fruit Advice!")
 try: 
@@ -33,22 +40,30 @@ try:
   if not fruit_choice: 
     streamlit.error("Please select a fruit to get information.") 
   else: 
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # normalises the data by applying a standard format to the table 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function = get_fruityvice_data(fruit_choice) 
     # displays the normalised fruityvice data 
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
     
 except URLError as e: 
   streamlit.error()
 
 # don't run anything past here 
-streamlit.stop() 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
+# Snowflake-related functions 
+def get_fruit_load_list(): 
+  with my_cnx.cursor() as my_cur: 
+    my_cur.execute("select * from fruit_load_list") 
+    return my_cur.fetchall() 
+
+# add a button to load the fruit 
+if streamlit.button('Get Fruit Load List'): 
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])  
+  my_data_rows = get_fruit_load_list() 
+  streamlit.dataframe(my_data_rows) 
+# my_cur = my_cnx.cursor()
+# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+# my_data_rows = my_cur.fetchall()
+
 streamlit.dataframe(my_data_rows)
 
 # allows user to add fruit 
