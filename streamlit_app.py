3977f4995ee8d36d411565ca59bae1350476d42d
Siying Huang <103796384+shua415@users.noreply.github.com>
2023-11-23 17:38:15 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
+# allows user to add fruit 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')") 
